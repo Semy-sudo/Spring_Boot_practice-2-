747b5e93a65b49a41da56721a5193d40d3fc95b8
songyein <songyein620@gmail.com>
2021-05-25 18:25:19 +0900

    lombok 추가

---
+++ src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,26 @@
+package com.example.firstproject.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity //DB가 해당 객체 인식 가능!
+@AllArgsConstructor //모든 필드에 대해 생성자 자동 생성
+@ToString
+public class Article {
+
+    @Id
+    @GeneratedValue //1,2,3... 자동생성 어노테이션
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+}
